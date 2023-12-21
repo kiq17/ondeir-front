b485bb5e00a41085be1a80a5980f049f4ef469bd
Caique <caiquedecastrosilva@yahoo.com.br>
2023-12-21 20:19:40 -0300

    feat: add input component to redefine pass component

--- src/components/RedfinePass/RedefinePass.tsx
+++ src/components/RedfinePass/RedefinePass.tsx
@@ -4,119 +4,163 @@ import * as yup from "yup";
 import { ValidationError } from "yup";
 import { redefine } from "../../services/api";
 import "../ChangePass/styleChangePass.css";
+import { Input } from "../Input";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/20/solid";
 
 export const passSchema = yup.object({
-    newPass: yup.string().required("Campo obrigatório")
-        .min(6, "Deve ter no mínimo 6 caracteres")
-        .max(20, "Deve ter no mínimo 20 caracteres"),
-    newPassConfirm: yup.string().required("Campo obrigatório")
-        .min(6, "Deve ter no mínimo 6 caracteres")
-        .max(20, "Deve ter no mínimo 20 caracteres")
+  newPass: yup
+    .string()
+    .required("Campo obrigatório")
+    .min(6, "Deve ter no mínimo 6 caracteres")
+    .max(20, "Deve ter no mínimo 20 caracteres"),
+  newPassConfirm: yup
+    .string()
+    .required("Campo obrigatório")
+    .min(6, "Deve ter no mínimo 6 caracteres")
+    .max(20, "Deve ter no mínimo 20 caracteres"),
+});
 
-})
+interface RedefinePassForm {
+  newPass: string;
+  newPassConfirm: string;
+}
 
 const RedefinePass = ({ params }) => {
-    const [loading, setLoading] = useState<boolean>(false);
-    const [newPass, setNewPass] = useState<string>("");
-    const [newPassConfirm, setNewPassConfirm] = useState<string>("");
-    const [errors, setErrors] = useState<Record<string, string>>({});
-    const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [formValue, setFormValue] = useState<RedefinePassForm>({
+    newPass: "",
+    newPassConfirm: "",
+  });
+  const [errors, setErrors] = useState<Record<string, string>>({});
+  const [toggle, setToggle] = useState<"text" | "password">("password");
+  const navigate = useNavigate();
 
-    const validateSchema = () => {
-        try {
-            passSchema.validateSync({
-                newPass,
-                newPassConfirm
-            }, { abortEarly: false })
-        } catch (error) {
-            const yupError = error as ValidationError;
-            const objectErros: Record<string, string> = {};
+  const validateSchema = () => {
+    try {
+      const { newPass, newPassConfirm } = formValue;
+      passSchema.validateSync(
+        {
+          newPass,
+          newPassConfirm,
+        },
+        { abortEarly: false }
+      );
+    } catch (error) {
+      const yupError = error as ValidationError;
+      const objectErros: Record<string, string> = {};
 
-            yupError.inner.forEach(error => {
-                if (!error.path) return;
+      yupError.inner.forEach((error) => {
+        if (!error.path) return;
 
-                objectErros[error.path] = error.message;
-            });
+        objectErros[error.path] = error.message;
+      });
 
-            setErrors(objectErros)
-            return true
-        }
+      setErrors(objectErros);
+      return true;
     }
+  };
 
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();
-        if (validateSchema()) return
-        if (newPass !== newPassConfirm) {
-            setErrors({ newPassConfirm: "As senhas estão diferente" })
-            return
-        }
-        try {
-            setLoading(true);
-            await redefine({ senha: newPassConfirm }, params);
-            navigate(`/`, { state: "active" })
-            setLoading(false);
-        } catch (error) {
-            setLoading(false);
-        }
-    };
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    if (validateSchema()) return;
+    const { newPass, newPassConfirm } = formValue;
+    if (newPass !== newPassConfirm) {
+      setErrors({ newPassConfirm: "As senhas estão diferente" });
+      return;
+    }
+    try {
+      setLoading(true);
+      await redefine({ senha: newPassConfirm }, params);
+      navigate(`/`, { state: "active" });
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <section className="change-pass conteiner">
-            <div className="change-pass-box">
-                <div className="change-pass-content">
-                    <div className="change-pass-texto">
-                        <h2>Redefinir senha</h2>
-                        <p>Preencha os dados abaixo com atenção</p>
-                    </div>
-                    <form className="change-pass-form" onSubmit={handleSubmit}>
-                        <div className="change-pass-first-form">
-                            <div className="input-box" style={{ marginBottom: "35px" }} onClick={(e) => {
-                                e.stopPropagation()
-                                const inp = ((e.target as HTMLDivElement).childNodes[0] as HTMLInputElement)
-                                inp.focus()
-                            }}>
-                                <input type="password"
-                                    name="Nova senha"
-                                    className="form-input"
-                                    placeholder=" "
-                                    onClick={(e) => e.stopPropagation()}
-                                    value={newPass}
-                                    onChange={(e) => setNewPass(e.target.value)}
-                                    autoComplete="off"
-                                />
-                                <label htmlFor="Nova senha" className="form-label">Nova senha</label>
-                                {errors.newPass && (
-                                    <p className="error-msg">{errors.newPass}</p>
-                                )}
-                            </div>
-                            <div className="input-box" style={{ marginBottom: "35px" }} onClick={(e) => {
-                                e.stopPropagation()
-                                const inp = ((e.target as HTMLDivElement).childNodes[0] as HTMLInputElement)
-                                inp.focus()
-                            }}>
-                                <input type="password"
-                                    name="Confirmar nova senha"
-                                    className="form-input"
-                                    placeholder=" "
-                                    onClick={(e) => e.stopPropagation()}
-                                    value={newPassConfirm}
-                                    onChange={(e) => setNewPassConfirm(e.target.value)}
-                                    autoComplete="off"
-                                />
-                                <label htmlFor="Confirmar nova senha" className="form-label">Confirmar nova senha</label>
-                                {errors.newPassConfirm && (
-                                    <p className="error-msg">{errors.newPassConfirm}</p>
-                                )}
-                            </div>
-                        </div>
-                        <div style={{ alignContent: "center", margin: "0 auto", width: "400px" }}>
-                            <button className="edit-profile-btn" type="submit">{loading ? "Carregando..." : "Salvar"}</button>
-                        </div>
-                    </form>
-                </div>
+  return (
+    <section className="change-pass conteiner">
+      <div className="change-pass-box">
+        <div className="change-pass-content">
+          <div className="change-pass-texto">
+            <h2>Redefinir senha</h2>
+            <p>Preencha os dados abaixo com atenção</p>
+          </div>
+          <form className="change-pass-form" onSubmit={handleSubmit}>
+            <div className="change-pass-first-form">
+              {["newPass", "newPassConfirm"].map((item, i) => {
+                return (
+                  <Input.Root
+                    key={i}
+                    handleOnChange={(value) => {
+                      setFormValue((prev) => ({ ...prev, [item]: value }));
+                    }}
+                    onBlur={() => {
+                      if (validateSchema()) return;
+
+                      setErrors({});
+                    }}
+                    value={formValue[item]}
+                    type={toggle}
+                    name={item}
+                    className={`${
+                      errors[item] ? "border-red-500" : "border-zinc-400"
+                    } mb-12 w-full`}
+                  >
+                    <Input.Label htmlFor={item}>
+                      {(function teste() {
+                        if (item == "newPass") return "Nova senha";
+                        if (item == "newPassConfirm")
+                          return "Confirmar nova senha";
+                      })()}
+                    </Input.Label>
+                    <Input.Error
+                      stantard="Atenção ao digitar"
+                      state={{
+                        value: formValue[item],
+                        error: errors[item],
+                      }}
+                    />
+                    <Input.Icon>
+                      {toggle == "text" && (
+                        <EyeIcon
+                          className="h-6 w-6 text-zinc-400 absolute top-4 right-2 cursor-pointer"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            setToggle("password");
+                          }}
+                        />
+                      )}
+                      {toggle == "password" && (
+                        <EyeSlashIcon
+                          className="h-6 w-6 text-zinc-400 absolute top-4 right-2 cursor-pointer"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            setToggle("text");
+                          }}
+                        />
+                      )}
+                    </Input.Icon>
+                  </Input.Root>
+                );
+              })}
             </div>
-        </section>
-    )
-}
+            <div
+              style={{
+                alignContent: "center",
+                margin: "0 auto",
+                width: "400px",
+              }}
+            >
+              <button className="edit-profile-btn" type="submit">
+                {loading ? "Carregando..." : "Salvar"}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default RedefinePass;
