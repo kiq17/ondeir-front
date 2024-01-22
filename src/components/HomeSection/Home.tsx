b4dfc20218a163fe8ed334671c053e479ea3260a
Caique <caiquedecastrosilva@yahoo.com.br>
2024-01-22 15:41:01 -0300

    styles: ui changes home section

--- src/components/HomeSection/Home.tsx
+++ src/components/HomeSection/Home.tsx
@@ -8,7 +8,6 @@ const Home = () => {
     const [loading, setLoading] = useState<boolean>(true);
     const imgRef = useRef<HTMLImageElement>(null);
     const isMed = useMediaQuery('(min-width: 1150px)');
-    const isMobile = useMediaQuery('(min-width: 550px)');
 
     useEffect(() => {
         if (imgRef.current) {
@@ -20,11 +19,12 @@ const Home = () => {
         }
     }, [imgRef, isMed])
     return (
-        <section className="banner conteiner">
-            <div className="banner-texto">
-                <h2>Viva a vida que você sempre quis viver.</h2>
-                {isMobile && <p>A vida é curta e o mundo é enorme, então comece agora!</p>}
-                <Link state={"melhor"} to={"/postagens"}>Melhores Lugares</Link>
+        <section className="flex justify-between items-center max-w-[1200px] m-auto">
+            <div className="max-w-[600px]">
+                <h2 className="mb-5 text-5xl font-bold">Viva a vida que você sempre quis viver.</h2>
+                <p className="text-2xl mb-6 text-zinc-500">A vida é curta e o mundo é enorme, então comece agora!</p>
+                {/* {isMobile && <p>A vida é curta e o mundo é enorme, então comece agora!</p>} */}
+                <Link className="btn" state={"melhor"} to={"/postagens"}>Melhores Lugares</Link>
             </div>
             {isMed && <div className={loading ? "img-banner" : "img-banner load"}>
                 <img src={bannerHome} alt="Garota andando por uma rua estreita com casas antigas, com uma mochila" loading="lazy" ref={imgRef} />
