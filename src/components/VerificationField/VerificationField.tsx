62c28ee271e91786d5fd456ee30c8f3271609699
Caique <caiquedecastrosilva@yahoo.com.br>
2024-04-01 19:44:06 -0300

    styles: add tailwind to verification component

--- src/components/VerificationField/VerificationField.tsx
+++ src/components/VerificationField/VerificationField.tsx
@@ -1,60 +1,66 @@
 import { useEffect, useRef, useState } from "react";
-import "./styleVerification.css";
 
 interface fieldProps {
-    handleOtp: (value: string[]) => void
+  handleOtp: (value: string[]) => void;
 }
 
 let currentOPTIndex = 0;
 
 const VerificationField = ({ handleOtp }: fieldProps) => {
-    const [otp, setOtp] = useState<string[]>(new Array(6).fill(""));
-    const [activeOTP, setActiveOTP] = useState<number>(0);
-    const inputRef = useRef<HTMLInputElement>(null);
+  const [otp, setOtp] = useState<string[]>(new Array(6).fill(""));
+  const [activeOTP, setActiveOTP] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = target;
-        const newOtp: string[] = [...otp]
-        newOtp[currentOPTIndex] = value.substring(value.length - 1);
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = target;
+    const newOtp: string[] = [...otp];
+    newOtp[currentOPTIndex] = value.substring(value.length - 1);
 
-        if (!value) setActiveOTP(currentOPTIndex - 1);
-        else setActiveOTP(currentOPTIndex + 1)
+    if (!value) setActiveOTP(currentOPTIndex - 1);
+    else setActiveOTP(currentOPTIndex + 1);
 
-        setOtp(newOtp)
-        handleOtp(newOtp)
-    }
+    if (newOtp.join("").length == 6) setLoading(true);
+    target.blur();
+    setOtp(newOtp);
+    handleOtp(newOtp);
+  };
 
-    const handleKey = ({ key }: React.KeyboardEvent<HTMLInputElement>, index: number) => {
-        currentOPTIndex = index
-        if (key === "Backspace") setActiveOTP(currentOPTIndex - 1)
-    }
+  const handleKey = (
+    { key }: React.KeyboardEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    currentOPTIndex = index;
+    if (key === "Backspace") setActiveOTP(currentOPTIndex - 1);
+  };
 
-    useEffect(() => {
-        inputRef.current?.focus()
-    }, [activeOTP])
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [activeOTP]);
 
-
-    return (
-        <div className="box-otp">
-            {otp.map((_, index) => {
-                return (
-                    <div key={index}>
-                        <input
-                            ref={index === activeOTP ? inputRef : null}
-                            type="number"
-                            className="input-otp"
-                            onChange={handleChange}
-                            value={otp[index]}
-                            onKeyDown={(e) => handleKey(e, index)}
-                        />
-                        {index === otp.length - 1 ? null : (
-                            <span className="input-otp-s" />
-                        )}
-                    </div>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className="flex m-auto justify-center items-center gap-4">
+      {otp.map((_, index) => {
+        return (
+          <div key={index} className={`${loading ? "cursor-not-allowed" : ""}`}>
+            <input
+              ref={index === activeOTP ? inputRef : null}
+              type="number"
+              className={`w-12 h-12 outline-none border-2 border-zinc-400 rounded-md text-center transition-all duration-300 bg-transparent text-lg font-semibold focus:border-2 focus:border-black ${
+                loading
+                  ? "bg-zinc-300 text-zinc-400 animate-pulse pointer-events-none"
+                  : ""
+              }`}
+              onChange={handleChange}
+              value={otp[index]}
+              onKeyDown={(e) => handleKey(e, index)}
+            />
+            {index === otp.length - 1 ? null : <span className="input-otp-s" />}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default VerificationField;
--- src/pages/Verification/index.tsx
+++ src/pages/Verification/index.tsx
@@ -1,5 +1,4 @@
-import { faExclamation } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { MegaphoneIcon } from "@heroicons/react/24/outline";
 import { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
@@ -7,126 +6,143 @@ import VerificationField from "../../components/VerificationField/VerificationFi
 import { checkCode, checkTemp, resendEmail } from "../../services/api";
 
 const VerficationPage = () => {
-    const [otp, setOtp] = useState<string[]>([])
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string>();
-    const [seconds, setSeconds] = useState(300);
-    const [cliques, setCliques] = useState(1);
-    const [user, setUsers] = useState();
-    const { temp } = useParams();
-    const navigate = useNavigate();
-    const location = useLocation()
+  const [otp, setOtp] = useState<string[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string>();
+  const [seconds, setSeconds] = useState(300);
+  const [cliques, setCliques] = useState(1);
+  const [user, setUsers] = useState();
+  const { temp } = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const t = async () => {
-        try {
-            const { data } = await checkTemp(temp);
-            setLoading(false)
-            setUsers(data.id)
-        } catch (error) {
-            setLoading(false)
-            navigate("/")
-        }
+  const t = async () => {
+    try {
+      const { data } = await checkTemp(temp);
+      setLoading(false);
+      setUsers(data.id);
+    } catch (error) {
+      setLoading(false);
+      // navigate("/")
+    }
+  };
+
+  const handleResend = () => {
+    if (seconds > 0) return;
+
+    setSeconds(0);
+
+    setCliques(cliques + 1);
+
+    switch (cliques) {
+      case 1:
+        setSeconds(10 * 60);
+        break;
+      case 2:
+        setSeconds(30 * 60);
+        break;
+      default:
+        setSeconds(2 * 60 * 60);
+        break;
     }
 
-    const handleResend = () => {
-        if (seconds > 0) return;
+    (async () => {
+      await resendEmail(user);
+    })();
+  };
+
+  useEffect(() => {
+    t();
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((seconds) => seconds - 1);
+      if (seconds === 0) {
+        clearInterval(interval);
 
         setSeconds(0);
+      }
+    }, 1000);
 
-        setCliques(cliques + 1);
+    return () => clearInterval(interval);
+  }, [cliques]);
 
-        switch (cliques) {
-            case 1:
-                setSeconds(10 * 60);
-                break;
-            case 2:
-                setSeconds(30 * 60);
-                break;
-            default:
-                setSeconds(2 * 60 * 60);
-                break;
-        }
+  const sendOtp = async () => {
+    const otpString = otp.join("").replace(",", "");
 
-        (async ()=>{
-            await resendEmail(user)
-        })()
-    }
-
-    useEffect(() => {
-        t();
-    }, []);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setSeconds(seconds => seconds - 1);
-            if (seconds === 0) {
-                clearInterval(interval);
-
-                setSeconds(0);
-            }
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, [cliques])
-
-
-
-    const sendOtp = async () => {
-        const otpString = otp.join("").replace(",", "")
-
-        /* if (otpString.length < 6) {
+    /* if (otpString.length < 6) {
             setError("Código incompleto")
             return
         } */
 
-        try {
-            if (location.state === "redefine") {
-                const { data } = await checkCode({ otp: otpString });
-                navigate(`/redefinir/${data.user}`)
-                return
-            }
-            await checkCode({ otp: otpString });
-            navigate(`/`, { state: "active" })
-        } catch (error) {
-            if (error instanceof AxiosError) {
-                setError(error.response?.data.message)
-            }
-        }
+    try {
+      if (location.state === "redefine") {
+        const { data } = await checkCode({ otp: otpString });
+        navigate(`/redefinir/${data.user}`);
+        return;
+      }
+      await checkCode({ otp: otpString });
+      navigate(`/`, { state: "active" });
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        setError(error.response?.data.message);
+      }
     }
+  };
 
-
-    if (loading) {
-        return (
-            <section className="profile conteiner" key={crypto.randomUUID()}>
-                <h3 style={{ margin: "0 auto", padding: "200px" }}>Carregando...</h3>
-            </section>
-        )
-    }
-
-    setInterval(() => {
-
-    }, 1000)
-    document.title = "OndeIr | Verificação"
-
+  if (loading) {
     return (
-        <section className="varification conteiner">
-            <div style={{ display: "flex", width: "100%", border: "2px solid red", padding: "15px", borderRadius: "8px", marginTop: "60px" }}>
-                <FontAwesomeIcon icon={faExclamation} color="red" size="xl" />
-                <p style={{ marginLeft: "10px", alignSelf: "flex-start" }}>
-                    <span style={{ fontSize: "18px", fontWeight: "700", color: "red", marginRight: "10px" }}>
-                        Atenção
-                    </span>Verifique seu E-mail.Esta página é temporária caso você saia dela por algum motivo realize novamente o seu login que outra página será gerada.</p>
-            </div>
-            <VerificationField handleOtp={value => setOtp(value)} />
-            <div style={{ display: "flex", flexDirection: "column", gap: "10px", alignItems: "center", width: "100%" }}>
-                {error && (
-                    <p className="error-msg" style={{ alignSelf: "flex-end", margin: "0 auto" }}>{error}</p>
-                )}
-                <button className="otp-btn" onClick={sendOtp} style={{ margin: "0 auto" }}>Checar código</button>
-                <button disabled={seconds > 0} onClick={handleResend} className="code-resend" style={{ alignSelf: "flex-end", margin: "0 auto", color: `${seconds == 0 ? "blue" : "gray"}` }}>{seconds > 0 ? `Reenviar código. Espere ${new Date(seconds * 1000).toISOString().slice(14, 19)}` : "Reenviar código."}</button>
-            </div>
-        </section>
-    )
-}
+      <section className="profile conteiner" key={crypto.randomUUID()}>
+        <h3 style={{ margin: "0 auto", padding: "200px" }}>Carregando...</h3>
+      </section>
+    );
+  }
+
+  setInterval(() => {}, 1000);
+  document.title = "OndeIr | Verificação";
+
+  return (
+    <section className="varification flex items-center justify-center h-screen conteiner relative overflow-hidden">
+      <div className="absolute top-0 left-40 w-[450px] h-[450px] bg-lime-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+      <div className="absolute top-20 right-40 w-[450px] h-[450px] bg-green-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+      <div className="absolute -bottom-10 right-96 w-[520px] h-[520px] bg-emerald-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+      <div className="w-[500px] h-[560px] bg-white z-50 rounded-lg flex flex-col gap-5 m-auto border-2 border-zinc-400 p-10">
+        <h1 className="font-bold text-3xl text-gr text-center">OndeIr</h1>
+        <h3 className="text-center font-normal text-lg">
+          Digite o código que foi enviado para o email{" "}
+          <span className="font-bold">*****as@gmail.com</span>
+        </h3>
+        <VerificationField handleOtp={(value) => setOtp(value)} />
+        {error && <p className="self-end m-auto text-red-500">{error}</p>}
+        <p className="text-center text-zinc-400">
+          Para proteger sua conta, não compartilhe esse código
+        </p>
+        <div className="bg-zinc-100 rounded-lg p-5">
+          <div className="flex items-center gap-2">
+            <MegaphoneIcon className="w-6 h-6" />
+            <p className="font-bold text-lg">Link temporário</p>
+          </div>
+          <p className="text-zinc-600 ml-8">
+            Caso você saia desta página por algum motivo realize novamente o seu
+            login para que outra link possa ser gerado.
+          </p>
+        </div>
+        <button
+          disabled={seconds > 0}
+          onClick={handleResend}
+          className="code-resend"
+          style={{ color: `${seconds == 0 ? "blue" : "gray"}` }}
+        >
+          {seconds > 0
+            ? `Reenviar código. Espere ${new Date(seconds * 1000)
+                .toISOString()
+                .slice(14, 19)}`
+            : "Reenviar código."}
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default VerficationPage;
