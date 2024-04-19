f518934aff1e1085e73791208b1846fe0a8102a1
Caique <caiquedecastrosilva@yahoo.com.br>
2024-04-18 23:37:57 -0300

    styles: add tailwind classes

--- src/components/Shared/Skeleton/CardSkeleton.tsx
+++ src/components/Shared/Skeleton/CardSkeleton.tsx
@@ -1,13 +1,8 @@
-import "./styleCardSkeleton.css";
 
-const CardSkeleton = ()=>{
-    return (
-        <div style={{height: "575px", width: "385px", borderRadius: "6px", backgroundColor: "#dbdbdb"}} className="skeleton-card">
-
-        </div>
-    )
-
-
-}
+const CardSkeleton = () => {
+  return (
+    <div className="w-96 h-[580px] bg-zinc-300 animate-pulse rounded-md"></div>
+  );
+};
 
 export default CardSkeleton;
